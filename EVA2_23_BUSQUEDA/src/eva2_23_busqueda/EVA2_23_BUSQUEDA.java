b7829b0eb2072bfa40febb52d286b4d70aa63b19
jesusmanuelgaytan09-collab <jesusmanuelgaytan09@gmail.com>
2025-12-01 16:41:07 -0600

    Add files via upload

---
+++ EVA2_23_BUSQUEDA/src/eva2_23_busqueda/EVA2_23_BUSQUEDA.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_23_busqueda;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author range
+ */
+public class EVA2_23_BUSQUEDA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner captu = new Scanner(System.in);
+        int valor, posi = -1;//-1 SIGNIFICA QUE NO LO ENCONTRAMOS
+        int datos[] = new int[10];
+         for (int i = 0; i < datos.length; i++){
+        datos[i] = (int) (Math.random()* 100);
+    }
+         for (int i = 0; i < datos.length; i++){
+        System.out.print("[" + datos [i] +"]");
+    }
+    System.out.println("");
+         System.out.println("valor a buscar: ");
+         valor = captu.nextInt();
+         // BUSQUEDA LINEAL
+     for (int i = 0; i < datos.length; i++){
+        if(valor == datos[i]) { 
+       posi = i; //regresamos donde lo encontramos
+       break;//ya lo encontraste, detengo la busqueda
+    } 
+    }
+         
+        System.out.println("posicion: " + posi);  
+    }
+    
+}
